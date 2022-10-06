06893661f16452d148a0aa51ccf1b7771f894984
Padmaja <109962029+PadmajaKothapalli@users.noreply.github.com>
2022-10-06 19:24:20 +0530

    Add files via upload

---
+++ Clock.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+const Clock=()=>{
+    let [currentTime, setCurrentTime]= React.useState(new Date().toLocaleDateString())
+    useEffect(()=>{
+        setInterval(()=>{
+            setCurrentTime(new Date(). toLocaleTimeString())
+        },[1000])
+    },[1000]);
+return(
+    <div className="container mt-5">
+        <pre>{JSON.stringify(currentTime)}</pre>
+        <div className="row">
+            <div className="col-md-5">
+                <div className="card">
+                    <div className="card-header">
+                        <h5>{currentTime}</h5>
+                    </div>
+                    <div className="card-body"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+}
+export default Clock;
